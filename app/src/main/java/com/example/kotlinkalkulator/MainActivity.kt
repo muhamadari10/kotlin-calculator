7bd64e89b67784fe20466941ea45531216ac7ed1
tada1011 <dandi.webdev@gmail>
2021-01-08 10:49:11 +0700

    first commit

---
+++ app/src/main/java/com/example/kotlinkalkulator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinkalkulator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+}
